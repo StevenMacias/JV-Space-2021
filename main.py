b25c959dd1eea3a399d47d7e03b83d445a3f3072
Steven Macias <ed.steven.macias@gmail.com>
2022-02-20 20:50:02 +0100

    Corrected structure of the code together with the students.

--- main.py
+++ main.py
@@ -2,38 +2,34 @@
 """
 Created on Thu Feb 17 17:36:39 2022
 
-@author: Jordi Castillo
+@author: Bernat Casas, Jordi Castillo, Nadia González, Steven Macías
 """
+from orbit import ISS
+from skyfield.api import load
 from sense_hat import SenseHat
+from datetime import datetime
+from pathlib import Path
+from time import sleep
+import csv
 
 sense = SenseHat() 
 sense.set_imu_config(True, False, False) 
 
-def getMagnetometerValues():
+def get_magnetometer_values():
     # Code to obtain values from the Magnetometer
     magnetometer_values = sense.get_compass_raw()
     # Code for filling magnetometer_values
     return magnetometer_values
 
-
-def main():
-   print("JV-Space")
-   mag = getMagnetometerValues()
-   print(mag)
-   
-if __name__ == "__main__":
-    main()
-
-
-#para archivar los datos del magnetómetro en un csv, incluyendo magnetismo, fecha y hora
-#también he intentado activar la función del mag. parando la función envez de con un temporizador
-
-import csv
-from sense_hat import SenseHat
-from datetime import datetime
-from pathlib import Path
-from time import sleep
-
+def get_iss_position():
+    # Obtain the current time `t`
+    t = load.timescale().now()
+    # Compute where the ISS is at time `t`
+    position = ISS.at(t)
+    # Compute the coordinates of the Earth location directly beneath the ISS
+    location = position.subpoint()
+    print(location)
+    
 def create_csv(data_file):
     with open(data_file, 'w') as f:
         writer = csv.writer(f)
@@ -45,26 +41,19 @@ def add_csv_data(data_file, data):
         writer = csv.writer(f)
         writer.writerow(data)
 
-sense = SenseHat()
+def main():
+   print("JV-Space")
+   mag = getMagnetometerValues()
+   print(mag)
+   base_folder = Path(__file__).parent.resolve()
+   data_file = base_folder/'data.csv'
 
-base_folder = Path(__file__).parent.resolve()
-data_file = base_folder/'data.csv'
+   create_csv(data_file)
+   for i in range(10):
+       row = (datetime.now(), sense.magnetism)
+       add_csv_data(data_file, row)
+       sleep(10)
+   
+if __name__ == "__main__":
+    main()
 
-create_csv(data_file)
-for i in range(10):
-    row = (datetime.now(), sense.magnetism)
-    add_csv_data(data_file, row)
-    sleep(10)
-    
-#y esto por si necesitamos saber donde se encuentra la foto hecha
-
-    from orbit import ISS
-from skyfield.api import load
-
-# Obtain the current time `t`
-t = load.timescale().now()
-# Compute where the ISS is at time `t`
-position = ISS.at(t)
-# Compute the coordinates of the Earth location directly beneath the ISS
-location = position.subpoint()
-print(location)
