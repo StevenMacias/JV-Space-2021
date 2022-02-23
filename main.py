9cef8cb236e1b686f7d3f940b7719b5ac67e9891
Bernat Casas <becama@jviladoms.cat>
2022-02-23 18:47:16 +0100

    added iteration to CSV file, edited experiment time and added some logs

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ Created on Thu Feb 17 17:36:39 2022
 
 @author: Bernat Casas, Jordi Castillo, Nadia González, Steven Macías
 """
+from datetime import datetime, timedelta
+from logzero import logger, logfile
 from sense_hat import SenseHat
 from skyfield.api import load
 from picamera import PiCamera
@@ -12,7 +14,7 @@ from time import sleep
 from orbit import ISS
 import csv
 import os
-from datetime import datetime, timedelta
+
 
 camera = PiCamera()
 camera.resolution = (1680, 1050)
@@ -35,37 +37,43 @@ def get_iss_position():
     
 def create_csv(data_file):
     with open(data_file, 'w') as f:
+        logger.info('Creating header of CSV file')
         writer = csv.writer(f)
-        header = ("datetime", "mag_x", "mag_y", "mag_z", "iss.pos_lat", "iss.pos_lon", "iss.pos_elv")
+        header = ("iter", "datetime", "mag_x", "mag_y", "mag_z", "iss.pos_lat", "iss.pos_lon", "iss.pos_elv")
         writer.writerow(header)
 
 def add_csv_data(data_file, data):
     with open(data_file, 'a') as f:
         writer = csv.writer(f)
+        logger.info('Writing row of data')
         writer.writerow(data)
 
 def main():
-   print("JV-Space")
+   logger.info("Executing JV-Space's program")
    sense = SenseHat() 
    sense.set_imu_config(True, False, False) 
    base_folder = Path(__file__).parent.resolve()
    data_file = base_folder/'data.csv'
+   logfile(base_folder/"events.log")
+   iteration = 0
 
    create_csv(data_file)
    # Create a `datetime` variable to store the start time
    start_time = datetime.now()
+   logger.info('Experiment started at {start_time}')
    # Create a `datetime` variable to store the current time
    # (these will be almost the same at the start)
    now_time = datetime.now()
    # Run a loop for 180 minutes
-   while (now_time < start_time + timedelta(minutes=180)):
+   while (now_time < start_time + timedelta(minutes=178)):
        mag = get_magnetometer_values(sense)
        iss_pos = get_iss_position()
-       row = (datetime.now(), mag["x"], mag["y"], mag["z"], iss_pos.latitude.degrees, iss_pos.longitude.degrees, iss_pos.elevation.km)
+       row = (iteration, datetime.now(), mag["x"], mag["y"], mag["z"], iss_pos.latitude.degrees, iss_pos.longitude.degrees, iss_pos.elevation.km)
        add_csv_data(data_file, row)
-       camera.capture(f'./images/image_{i:03d}.jpg')
+       camera.capture(f'./images/image_{iteration:03d}.jpg')
        sleep(15)
        now_time = datetime.now()
+       iteration = iteration + 1
    
 if __name__ == "__main__":
     main()
