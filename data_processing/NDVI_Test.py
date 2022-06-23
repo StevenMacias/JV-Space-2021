7dc146b23e054d340681ecfd6320e41b4dc6260d
Jordi Castillo <castillolorenzojordi@gmail.com>
2022-06-23 18:37:09 +0200

    Code to generate NDVI/mag_val chart

---
+++ data_processing/NDVI_Test.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jun 18 13:39:41 2022
+
+@author: Jordi Castillo
+"""
+
+import pandas as pd
+pd.plotting.register_matplotlib_converters()
+import matplotlib.pyplot as plt
+%matplotlib inline
+import seaborn as sns
+
+plt.figure(figsize=(6,48))
+
+correlation_filepath = "../input/processed-data/processed_data.csv"
+correlation_data = pd.read_csv(correlation_filepath)
+sns.regplot(x=correlation_data['mag_val'], y=correlation_data['NDVI_val'])
+
